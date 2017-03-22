854451e0f56e4af895350eabe23b5423c2b0485a
JoseAntonioLpz <joseantoniolopez241197@gmail.com>
2017-03-22 11:47:31 +0100

    first commit

---
+++ consultasMedicasApp/app/src/main/java/com/example/josea/consultasmedicas/MainView.java
@@ -0,0 +1,13 @@
+package com.example.josea.consultasmedicas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_view);
+    }
+}
