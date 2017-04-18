b76286fbd8cd0f463d32f3b895ac2f63c31b539f
JoseAntonioLpz <joseantoniolopez241197@gmail.com>
2017-04-18 09:46:30 +0200

    18/04/2017

---
+++ webPrincipal/footer.php
@@ -0,0 +1,43 @@
+<!-- Footer -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-xs-4 col-sm-4 col-md-4">
+                <div class="logoFooter">
+                    <h2>Logo</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xs-4 col-sm-4 col-md-4">
+                <div class="contactoFooter">
+                    <div class="separadorFooter"></div>
+                    <p><i class="fa fa-map-pin"></i> C/Gerónimo de Rueda 18, Cúllar Vega, Granada</p>
+                    <p><i class="fa fa-phone"></i> Phone (Spain) : +34 627 190 552</p>
+                    <p><i class="fa fa-envelope"></i> E-mail : joseantoniolopez241197@gmail.com</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-xs-4 col-sm-4 col-md-4">
+                <div class="redesSocialesFooter">
+                    <div class="separadorFooterRedes"></div>
+                    <a href="https://www.facebook.com/joseantonio.lopezlopez.106"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+                    <a href="https://twitter.com/JosAntLpz?lang=es"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                    <a href="https://github.com/JoseAntonioLpz"><i class="fa fa-github" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-lg-12 col-xs-12 col-sm-12 col-md-12">
+                <div class="copyright">
+                    © Copyright 1997-2017 Jose Antonio López - All Rights Reserved
+                </div> 
+            </div>    
+        </div>
+    </div>
+</footer>
+    <!--jQuery-->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ webPrincipal/nav.php
@@ -0,0 +1,28 @@
+<header>
+    <nav>
+        <div class="container head">
+            <div class="row">
+                <div class="col-lg-2 col-xs-2 col-sm-2 col-md-2">
+                    <div class="logo"></div>
+                </div>
+                
+                <div class="col-lg-10 col-xs-10 col-sm-10 col-md-10">
+                    <div class="navigation">
+                        <div class="login">
+                            <a href="login.php" class="nav">Login</a>
+                        </div>
+                        <div class="inicio">
+                            <a href="#inicio" class="nav">Inicio</a>
+                        </div>
+                        <div class="servicios">
+                            <a href="#servicios" class="nav">Servicios</a>
+                        </div>
+                        <div class="noticias">
+                            <a href="#noticias" class="nav">Noticias</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
